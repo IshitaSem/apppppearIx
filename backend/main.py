a0226302226079093d6f57925c4bc9fcf8df977a
IshitaSem <145051493+IshitaSem@users.noreply.github.com>
2026-04-14 22:29:13 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ import uvicorn
 
 from database import test_mongodb_connection
 
-from upload import router as upload_router
+# from upload import router as upload_router
 from auth import router as auth_router
 from wardrobe import router as wardrobe_router
 from outfit import router as outfit_router
@@ -25,7 +25,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Make sure uploads folder exists before mounting
 os.makedirs("uploads", exist_ok=True)
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
@@ -53,12 +52,10 @@ def health():
 @app.get("/test-db")
 def test_db():
     success = test_mongodb_connection()
-    return {
-        "mongodb_connected": success
-    }
+    return {"mongodb_connected": success}
 
 
-app.include_router(upload_router)
+# app.include_router(upload_router)
 app.include_router(auth_router)
 app.include_router(wardrobe_router)
 app.include_router(outfit_router)
@@ -69,9 +66,4 @@ app.include_router(fashion_tools_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
-    )
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
